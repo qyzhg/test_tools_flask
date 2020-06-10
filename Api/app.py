c68a50d746305196fa36b17c8333d0cb917aef41
qyzhg <qyzhg@163.com>
2020-06-10 23:20:42 +0800

    执行测试的前端页面基本完成

--- Api/app.py
+++ Api/app.py
@@ -1,26 +1,29 @@
 import os
 import sys
+import csv
+
 try:
-    from flask import Flask, request, render_template, redirect,url_for
+    from flask import Flask, request, render_template, redirect, url_for
 except ImportError:
     os.system('pip install flask')
     from flask import Flask, request, render_template, redirect, url_for
-sys.path.append(os.path.abspath('../'))
+# sys.path.append(os.path.abspath('../'))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from Api.getyaml.json_to_yaml import json_to_yaml, make_locustfile, make_apifile
 from Api.public.WriteJson import write_json
 from Api.public.start import start_project
-from Api.settings import CASE_DIR,HOST,PROJECT_NAME
+from Api.settings import CASE_DIR, HOST, PROJECT_NAME,EMAIL_ADDRESS
 from Api.public.msg import Msg
 from Api.public.create_script import create_script
 
 app = Flask(__name__)
 
 
-
+@app.route('/index', methods=['POST', 'GET'])
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'GET':
-        return render_template('index.html',PROJECT_NAME = PROJECT_NAME)
+        return render_template('index.html', PROJECT_NAME=PROJECT_NAME)
     elif request.method == 'POST':
         # 获取表单内容
         url = request.form.get('url')
@@ -33,26 +36,23 @@ def index():
         remark = request.form.get('remark')
         json_data = request.form.get('json_data')
         menu = request.form.get('menu')
-
         if name == '':
             context = Msg([{'2': '如果未现在自动生成接口名为必填项'}])
-            return render_template('index.html',**context,PROJECT_NAME = PROJECT_NAME)
-
+            return render_template('index.html', **context, PROJECT_NAME=PROJECT_NAME)
         elif url == '':
-            context = Msg([{'2':'接口地址为必填项'}])
-            return render_template('index.html',**context,PROJECT_NAME = PROJECT_NAME)
-
+            context = Msg([{'2': '接口地址为必填项'}])
+            return render_template('index.html', **context, PROJECT_NAME=PROJECT_NAME)
         elif remark == '':
             context = Msg([{'2': '备注为必填项'}])
-            return render_template('index.html',**context,PROJECT_NAME = PROJECT_NAME)
+            return render_template('index.html', **context, PROJECT_NAME=PROJECT_NAME)
         # 写入json文件
         write_json(json_data=json_data)
         # yaml输出路径
         case_path = os.path.join(CASE_DIR, name) + '.yaml'
         if os.path.isfile(case_path):
-            #已存在
+            # 已存在
             context = Msg([{'1': f'该测试用例已存在，如需再次生成请手动将{case_path}删除或者重命名后再操作'}])
-            return render_template('index.html', **context,PROJECT_NAME = PROJECT_NAME)
+            return render_template('index.html', **context, PROJECT_NAME=PROJECT_NAME)
         else:
             # 执行操作
             L = list()
@@ -75,11 +75,11 @@ def index():
                     c = make_apifile(name=name, remark=remark)
                     L.append(c)
                 context = Msg(L)
-                return render_template('index.html',**context,PROJECT_NAME = PROJECT_NAME)
+                return render_template('index.html', **context, PROJECT_NAME=PROJECT_NAME)
             else:
                 L.append(a)
                 context = Msg(L)
-                return render_template('index.html',**context,PROJECT_NAME = PROJECT_NAME)
+                return render_template('index.html', **context, PROJECT_NAME=PROJECT_NAME)
 
 
 @app.route('/start', methods=['POST', 'GET'])
@@ -90,22 +90,41 @@ def start():
         name = request.form.get('name')
         if name == '':
             danger = ['请输入项目名!']
-            return render_template('start.html',danger = danger)
+            return render_template('start.html', danger=danger)
         a = start_project(name)
         context = Msg(a)
-        return render_template('start.html',**context)
+        return render_template('start.html', **context)
 
 
-@app.route('/cs',methods=['POST','GET'])
+@app.route('/cs', methods=['POST', 'GET'])
 def cs():
     if request.method == 'GET':
-        return render_template('cs.html',PROJECT_NAME = PROJECT_NAME)
+        return render_template('cs.html', PROJECT_NAME=PROJECT_NAME)
     elif request.method == 'POST':
         menu = request.form.get('menu')
-        a = create_script(PROJECT_NAME,menu)
+        a = create_script(PROJECT_NAME, menu)
         # print(a)
         context = Msg(a)
-        return render_template('cs.html',PROJECT_NAME = PROJECT_NAME,**context)
+        return render_template('cs.html', PROJECT_NAME=PROJECT_NAME, **context)
+
+
+@app.route('/runtest',methods=['POST','GET'])
+def runtest():
+    if request.method == 'GET':
+        email_list = list()
+        with open(EMAIL_ADDRESS, "r",encoding='utf-8') as f:
+            csv_read = csv.reader(f)
+            for line in csv_read:
+                d = {
+                    'email':line[0],
+                    'name':line[1],
+                    'duty':line[2],
+                    'state':line[3]
+                }
+                email_list.append(d)
+        return render_template('runtest.html',PROJECT_NAME = PROJECT_NAME,email_list=email_list[1:])
+
+
 
 if __name__ == '__main__':
     app.run()
--- Api/public/start.py
+++ Api/public/start.py
@@ -13,74 +13,82 @@
 '''
 
 import os
-import sys
 from shutil import copyfile
 
-from Api.settings import PARENT_LOCUSTFILE_DIR, PARENT_CASE_DIR, PARENT_TEST_API_DIR, PARENT_REPORT_DIR ,TEMPLATE_DIR
+from Api.settings import PARENT_LOCUSTFILE_DIR, PARENT_CASE_DIR, PARENT_TEST_API_DIR, PARENT_REPORT_DIR, TEMPLATE_DIR
 
 
 def start_project(startname):
     if not os.path.isdir(PARENT_CASE_DIR):
-        #创建测试用例目录
+        # 创建测试用例目录
         os.makedirs(PARENT_CASE_DIR)
     if not os.path.isdir(PARENT_TEST_API_DIR):
-        #创建接口测试脚本目录
+        # 创建接口测试脚本目录
         os.makedirs(PARENT_TEST_API_DIR)
     if not os.path.isdir(PARENT_LOCUSTFILE_DIR):
-        #创建性能测试脚本目录
+        # 创建性能测试脚本目录
         os.makedirs(PARENT_LOCUSTFILE_DIR)
     if not os.path.isdir(PARENT_REPORT_DIR):
-        #创建测试报告目录
+        # 创建测试报告目录
         os.makedirs(PARENT_REPORT_DIR)
 
     start = startname.upper()
-    locust_dir, test_api_dir, case_dir ,report_dir= (
-                                                        os.path.join(PARENT_LOCUSTFILE_DIR, start)), (
-                                                        os.path.join(PARENT_TEST_API_DIR, 'test_'+start)), (
-                                                        os.path.join(PARENT_CASE_DIR, start)),(
-                                                        os.path.join(PARENT_REPORT_DIR,start))
+    locust_dir, test_api_dir, case_dir, report_dir = (
+                                                         os.path.join(PARENT_LOCUSTFILE_DIR, start)), (
+                                                         os.path.join(PARENT_TEST_API_DIR, 'test_' + start)), (
+                                                         os.path.join(PARENT_CASE_DIR, start)), (
+                                                         os.path.join(PARENT_REPORT_DIR, start))
     a = make_locust_dir(locust_dir)
     b = make_test_api_dir(test_api_dir)
     c = make_case_dir(case_dir)
     d = make_report_dir(report_dir)
-    return [a,b,c,d]
+    return [a, b, c, d]
 
 
 def make_locust_dir(locust_dir):
     if os.path.isdir(locust_dir):
-        return {'1':'新建locust项目失败！{locust_dir}目录已存在，请检查路径！'.format(locust_dir=locust_dir)}
+        print('新建locust项目失败！{locust_dir}目录已存在，请检查路径！'.format(locust_dir=locust_dir))
+        return {'1': '新建locust项目失败！{locust_dir}目录已存在，请检查路径！'.format(locust_dir=locust_dir)}
     else:
         # 新建locust项目目录
         os.makedirs(locust_dir)
         make_locust_files(locust_dir)
-        return {'0':'新建locust目录成功!'}
+        print(f'新建locust目录成功!地址为{locust_dir}')
+        return {'0': '新建locust目录成功!'}
 
 
 def make_test_api_dir(test_api_dir):
     if os.path.isdir(test_api_dir):
-        return {'1':'新建test_api项目失败！{test_api_dir}目录已存在，请检查路径！'.format(test_api_dir=test_api_dir)}
+        print('新建test_api项目失败！{test_api_dir}目录已存在，请检查路径！'.format(test_api_dir=test_api_dir))
+        return {'1': '新建test_api项目失败！{test_api_dir}目录已存在，请检查路径！'.format(test_api_dir=test_api_dir)}
     else:
         # 新建test_api项目目录
         os.makedirs(test_api_dir)
         make_test_api_files(test_api_dir)
-        return {'0':'新建test_api目录成功!'}
+        print(f'新建test_api目录成功!地址为{test_api_dir}')
+        return {'0': '新建test_api目录成功!'}
 
 
 def make_case_dir(case_dir):
     if os.path.isdir(case_dir):
-        return {'1':'新建case_dir项目失败！{case_dir}目录已存在，请检查路径！'.format(case_dir=case_dir)}
+        print(f'新建case_dir项目失败！{case_dir}目录已存在，请检查路径！')
+        return {'1': '新建case_dir项目失败！{case_dir}目录已存在，请检查路径！'.format(case_dir=case_dir)}
     else:
         # 新建case_dir项目目录
         os.makedirs(case_dir)
-        return {'0':'新建case_dir目录成功!'}
+        print('新建case_dir目录成功!')
+        return {'0': '新建case_dir目录成功!'}
+
 
 def make_report_dir(report_dir):
     if os.path.isdir(report_dir):
-        return {'1':f'新建report_dir项目失败！{report_dir}目录已存在，请检查路径！'}
+        print(f'新建report_dir项目失败！{report_dir}目录已存在，请检查路径！')
+        return {'1': f'新建report_dir项目失败！{report_dir}目录已存在，请检查路径！'}
     else:
         # 新建report_dir项目目录
         os.makedirs(report_dir)
-        return {'0':f'新建report_dir目录成功!'}
+        print(f'新建report_dir目录成功!{report_dir}')
+        return {'0': f'新建report_dir目录成功!'}
 
 
 def make_locust_files(locust_dir):
