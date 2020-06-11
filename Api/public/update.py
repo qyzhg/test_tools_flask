ba26603cd120cc5e569d78f1e3ab8c0de235368a
qyzhg <qyzhg@163.com>
2020-06-11 13:48:35 +0800

    升级功能

--- Api/public/update.py
+++ Api/public/update.py
@@ -18,19 +18,23 @@ from Api import settings
 
 def Update():
     # if update.upper() == 'TOOLS':
-    print('更新工具有丢失数据的风险！请将case妥善备份！！请将case备份后继续！！！\nY:继续（默认）\nN:取消')
-    while True:
-        try:
-            if (a := input('\n').upper()) == 'Y' or a == '':
-                toolsdir = (os.path.dirname(settings.BASE_DIR))
-                os.system(f'cd {toolsdir} && git fetch --all && git reset --hard origin/master && git pull')
-                # print('挡板触发')
-                return
-            elif a == 'N':
-                print('操作已取消!!')
-                return
-            else:
-                print('输入错误，请重新输入')
-        except SyntaxError:
-            print('请更新python版本到3.8以上在重试')
-            return
+    # print('更新工具有丢失数据的风险！请将case妥善备份！！请将case备份后继续！！！\nY:继续（默认）\nN:取消')
+    # while True:
+    #     try:
+    #         if (a := input('\n').upper()) == 'Y' or a == '':
+    #             toolsdir = (os.path.dirname(settings.BASE_DIR))
+    #             os.system(f'cd {toolsdir} && git fetch --all && git reset --hard origin/master && git pull')
+    #             # print('挡板触发')
+    #             return
+    #         elif a == 'N':
+    #             print('操作已取消!!')
+    #             return
+    #         else:
+    #             print('输入错误，请重新输入')
+    #     except SyntaxError:
+    #         print('请更新python版本到3.8以上在重试')
+    #         return
+    toolsdir = (os.path.dirname(settings.BASE_DIR))
+    os.system(f'cd {toolsdir} && git fetch --all && git reset --hard origin/master && git pull')
+    print('更新成功')
+    return [{'0':'更新成功！'}]
